e5f496e6377d060d8cf313717172b234d0c18fc3
Surajbarole <surajbarole1@gmail.com>
2023-08-19 12:56:40 +0530

    Second Commit

--- App.js
+++ App.js
@@ -41,19 +41,19 @@ function searchMovies() {
         });
       });
   } else {
-    movieContainer.innerHTML = "";
+    movieContainer.innerHTML ="";
     fetchNowPlayingMovies();
   }
 }
 
 // ! --STEP 3-----------------
-const movieContainer = document.querySelector(".movie-container");
+const movieContainer =document.querySelector(".movie-container");
 
 const apiKey = "eda08b1cc545e927809e4bfb1fed1676";
 const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
 
 fetch(nowPlayingUrl)
-  .then((response) => response.json())
+  .then((response) =>response.json())
   .then((data) => {
     const movies = data.results;
     movies.forEach((movie) => {
@@ -68,19 +68,18 @@ function createMovieCard(movie) {
 
   const movieImage = document.createElement("img");
   movieImage.src = `https://image.tmdb.org/t/p/w300${movie.poster_path}`;
-  movieImage.alt = `${movie.title} Poster`;
+  movieImage.alt =`${movie.title} Poster`;
 
-  const movieTitle = document.createElement("h2");
-  movieTitle.textContent = movie.title;
+  const movieTitle =document.createElement("h2");
+  movieTitle.textContent =movie.title;
 
-  const movieLanguage = document.createElement("p");
+  const movieLanguage =document.createElement("p");
   movieLanguage.classList.add("lang");
   movieLanguage.textContent = `${movie.original_language}`;
 
-  const movieRatings = document.createElement("p");
+  const movieRatings =document.createElement("p");
   movieRatings.classList.add("ratingmovie");
   movieRatings.textContent = `${movie.vote_average}`;
-
   movieCard.appendChild(movieImage);
   movieCard.appendChild(movieTitle);
   movieCard.appendChild(movieLanguage);
@@ -94,15 +93,15 @@ function createMovieCard(movie) {
 
 // !  ----GENRESLIST ---------
 
-const genreList = document.querySelector(".genre-list");
+const genreList=document.querySelector(".genre-list");
 
-const genresUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
+const genresUrl =`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
 
 fetch(genresUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    const genres = data.genres;
-    genres.forEach((genre) => {
+  .then((response) =>response.json())
+  .then((data) =>{
+    const genres=data.genres;
+    genres.forEach((genre)=> {
       const genreButton = createGenreButton(genre);
       genreList.appendChild(genreButton);
     });
@@ -112,9 +111,58 @@ function createGenreButton(genre) {
   const genreButton = document.createElement("p");
   genreButton.textContent = genre.name;
   genreButton.classList.add("genre-button");
-
-  genreButton.addEventListener("click", () => {});
-
+  genreButton.addEventListener("click", () => { });
   return genreButton;
 }
 
+
+
+// !-----PAYMENT---------
+const checkout =document.querySelector('.checkout');
+const movieName =document.querySelector('.movie-name');
+const ticketPrice =document.querySelector('.ticket-price');
+const numTicketsInput =document.getElementById('numTickets');
+const convenienceFee =document.querySelector('.convenience-fee');
+const subtotal =document.querySelector('.subtotal');
+const paymentMethod =document.getElementById('paymentMethod');
+const payButton =document.querySelector('.pay-button');
+const paymentForm =document.querySelector('form');
+const movie ={
+    title: 'Sample Movie',
+    ticketPrice: 500,
+};
+
+movieName.textContent =movie.title;
+ticketPrice.textContent =`Rs.${movie.ticketPrice.toFixed(2)}`;
+
+numTicketsInput.addEventListener('input',updateSummary);
+paymentMethod.addEventListener('change',updateSummary);
+
+function updateSummary() {
+    const numTickets =parseFloat(numTicketsInput.value);
+    const feePercentage =1.75;
+    const fee = (numTickets *movie.ticketPrice *feePercentage)/100;
+    const total = numTickets * movie.ticketPrice +fee;
+
+    convenienceFee.textContent = `Rs.${fee.toFixed(2)}`;
+    subtotal.textContent = `Rs.${total.toFixed(2)}`;
+}
+
+paymentForm.addEventListener('submit', handlePayment);
+function handlePayment(event) {
+    event.preventDefault();
+    const paymentSuccessful = simulatePayment();
+    if (paymentSuccessful) {
+        showConfirmation();
+    } else {
+        alert('Payment failed. Please try again.');
+    }
+}
+
+function simulatePayment() {
+    return Math.random() < 0.8;
+}
+
+function showConfirmation() {
+    checkout.innerHTML = '<h2>Payment Successful!</h2><p>Thank you for booking tickets.</p>';
+}
